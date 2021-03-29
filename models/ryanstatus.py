bdb931d8b50be2874bc77cadf3e4004374fc6edf
Ryan Heitmann <ryan.heitmann@gmail.com>
2021-03-29 15:39:19 +1300

    Implementing data access models

--- models/ryanstatus.py
+++ models/ryanstatus.py
@@ -8,3 +8,5 @@ A class that gathers any data related to Ryan
 class RyanStatus:
     def __init__(self):
         self.current_status = data_access.get_status()["message"]
+        self.current_projects = data_access.get_status()["projects"]
+        self.current_reads = data_access.get_status()["current_reads"]
